0fd42dd7be32f5047697c42a439ee319325275d7
Muhammed Abdrahman <muhammed03amin@gmail.com>
2021-05-27 16:14:11 +0600

    feat: solve subtask3

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -4,6 +4,34 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        var result = ArrayList<String>();
+        var openBrackets: String = "{«(<[";
+        var closeBrackets = "}»)>]";
+        var counter = 0;
+        var smallString = "";
+        var symbNumb = 0;
+
+        for (i in inputString.indices) {
+            symbNumb = openBrackets.indexOf(inputString[i]);
+            if (symbNumb != -1) {
+                counter = 0;
+                smallString = "";
+                for (j in i + 1 until inputString.length) {
+                    if (inputString[j] == closeBrackets[symbNumb] && counter == 0) {
+                        break
+                    }
+                    smallString += inputString[j];
+                    if (inputString[j] == openBrackets[symbNumb]) {
+                        counter++;
+                    }
+                    if (inputString[j] == closeBrackets[symbNumb]) {
+                        counter--;
+                    }
+                }
+                result.add(smallString);
+            }
+        }
+
+        return result.toTypedArray();
     }
 }
